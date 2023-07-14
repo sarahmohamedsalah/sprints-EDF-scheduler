4cf615ce5d5bedea72b2f099260e6589cc52a036
Hossam elwahsh <hacker2894@gmail.com>
2023-07-14 04:38:49 +0300

    update: task.h

--- FreeRTOS/Source/include/task.h
+++ FreeRTOS/Source/include/task.h
@@ -333,6 +333,17 @@ is used in assert() statements. */
 							void * const pvParameters,
 							UBaseType_t uxPriority,
 							TaskHandle_t * const pxCreatedTask ) PRIVILEGED_FUNCTION;
+
+    #if( configUSE_EDF_SCHEDULER == 1 )
+        BaseType_t xTaskPeriodicCreate( TaskFunction_t pxTaskCode,
+                                        const char * const pcName,		/*lint !e971 Unqualified char types are allowed for strings and single characters only. */
+                                        const configSTACK_DEPTH_TYPE usStackDepth,
+                                        void * const pvParameters,
+                                        UBaseType_t uxPriority,
+                                        TaskHandle_t * const pxCreatedTask,
+                                        TickType_t taskPeriod) PRIVILEGED_FUNCTION;
+    #endif
+
 #endif
 
 /**
